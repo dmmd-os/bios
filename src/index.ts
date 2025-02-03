4fa422848b85999cacbe64605f36e00b1bafd986
DmmD GM <dm12332131mdgaming@gmail.com>
2025-02-02 20:05:16 -0500

    finished basic interaction with image terminal

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ document.addEventListener("keydown", (key: KeyboardEvent) => {
 	if(key.ctrlKey && key.key === "r") {}
 	else if(key.ctrlKey && key.key === "R") {}
 	else if(key.ctrlKey && key.key === "I") {}
-	else imageTerminal.key(key); // some spoiled brat wants me to escape ctrl r and ctrl shift i
+	else imageTerminal.key(key); // some spoiled goos wants me to escape ctrl r and ctrl shift i
 });
--- src/scripts/image_terminal.ts
+++ src/scripts/image_terminal.ts
@@ -2,12 +2,16 @@
 // @ts-ignore
 import "../styles/image_terminal.css";
 
-// Defines type
+// Defines image terminal command
 export type ImageTerminalCommand = {
 	command: string;
-	flags: { [ flag: string ]: string; };
+	flags: { [ flag: string ]: string[]; };
 	parameters: string[];
+	parts: string[];
 };
+
+// Defines image terminal error
+export class ImageTerminalError extends Error {}
 
 // Defines image terminal
 export class ImageTerminal {
@@ -19,13 +23,15 @@ export class ImageTerminal {
 	private _highlight: HTMLDivElement;
 	private _history: Set<string>;
 	private _input: string;
-	private _tasks: Set<Promise<any>>;
+	private _pause: boolean;
+	private _tasks: Set<Promise<void>>;
 	private _textbox: HTMLDivElement;
 	private _timeline: string[] | null;
 	private _underline: HTMLDivElement;
-	readonly commands: Map<string, () => Promise<void>>;
+	readonly commands: Map<string, (command: ImageTerminalCommand) => Promise<void>>;
 	readonly frame: HTMLDivElement;
-	readonly states: Map<string, any>;
+	readonly version: string;
+	key: (key: KeyboardEvent) => Promise<void>;
 
 	// Constructs terminal
 	constructor() {
@@ -37,13 +43,64 @@ export class ImageTerminal {
 		this._highlight = document.createElement("div");
 		this._history = new Set();
 		this._input = "";
+		this._pause = false;
 		this._tasks = new Set();
 		this._textbox = document.createElement("div");
 		this._timeline = [];
 		this._underline = document.createElement("div");
 		this.commands = new Map();
 		this.frame = document.createElement("div");
-		this.states = new Map();
+		this.key = this.listener;
+		this.version = "1.0.0";
+
+		// Initializes commands
+		const commands: { alias: string[], executable: (command: ImageTerminalCommand) => Promise<void> }[] = [
+			// Clears terminal
+			{
+				alias: [ "clear", "cls" ],
+				executable: async () => {
+					this.clear();
+				}
+			},
+
+			// Displays help menu
+			{
+				alias: [ "cmds", "commands", "h", "help" ],
+				executable: async () => {
+					this.print("Available commands:");
+					const keys = Array.from(this.commands.keys());
+					this.print(keys.join(", "));
+					this.print("\nFor more instructions, please reference documentation at [insert url]");
+				}
+			},
+
+			// Prints content to terminal
+			{
+				alias: [ "echo", "print" ],
+				executable: async (command) => {
+					this.print(command.parameters.join(" "));
+				}
+			},
+
+			// Reloads window
+			{
+				alias: [ "r", "reboot" ],
+				executable: async () => {
+					location.reload();
+				}
+			},
+
+			// Prints version
+			{
+				alias: [ "v", "version" ],
+				executable: async () => {
+					this.print(this.version);
+				}
+			}
+		];
+		for(let i = 0; i < commands.length; i++) {
+			for(let j = 0; j < commands[i].alias.length; j++) this.commands.set(commands[i].alias[j], commands[i].executable);
+		}
 
 		// Initializes highlight
 		this._highlight.classList.add("highlight");
@@ -77,9 +134,7 @@ export class ImageTerminal {
 	// Modifies anchor
 	set anchor(anchor: number) {
 		// Updates anchor
-		const updated = Math.min(Math.max(anchor, 0), this._input.length);
-		if(this._anchor === updated) return;
-		this._anchor = updated;
+		this._anchor = Math.min(Math.max(anchor, 0), this._input.length);
 
 		// Renders
 		this.render();
@@ -100,10 +155,8 @@ export class ImageTerminal {
 
 	// Modifies chrono
 	set chrono(chrono: number) {
-		// Updates chrono 
-		const updated = Math.min(Math.max(chrono, 0), this._history.size);
-		if(this._chrono === updated) return;
-		this._chrono = updated;
+		// Updates chrono
+		this._chrono = Math.min(Math.max(chrono, 0), this._history.size);
 
 		// Synchronizes timeline
 		if(this._timeline === null) this._timeline = Array.from(this._history);
@@ -138,30 +191,47 @@ export class ImageTerminal {
 	// Modifies cursor
 	set cursor(cursor: number) {
 		// Updates cursor
-		const updated = Math.min(Math.max(cursor, 0), this._input.length);
-		if(this._cursor === updated) return;
-		this._cursor = updated;
+		this._cursor = Math.min(Math.max(cursor, 0), this._input.length);
 
 		// Renders
 		this.render();
 		this.bump();
 	}
 
+	// Retrieves input
+	get input() {
+		// Returns input
+		return this._input;
+	}
+
+	// Inserts input at cursor selection
+	async insert(insertion: string): Promise<void> {
+		// Splits insertion
+		const split = insertion.indexOf("\n");
+		const before = this.input.slice(0, Math.min(this.anchor, this.cursor));
+		const inserted = split === -1 ? insertion : insertion.slice(0, split);
+		const after = this.input.slice(Math.max(this.anchor, this.cursor));
+		const appended = split === -1 ? "" : insertion.slice(split);
+
+		// Writes insertion
+		await this.write(before + inserted + after + appended, before.length + inserted.length);
+	}
+
 	// Handles keypress
-	async key(key: KeyboardEvent): Promise<void> {
-		// Guards against meta and alt keys
+	async listener(key: KeyboardEvent): Promise<void> {
+		// Defines guards
+		if(this.pause) return;
 		if(key.metaKey || key.altKey) return;
-		key.preventDefault();
 
 		// Defines utility keys
-		if(key.key === "Enter") this.write(this.input + "\n");
+		if(key.key === "Enter") await this.write(this.input + "\n");
 		else if(key.key === "Backspace" || key.key === "Delete") {
 			if(this.anchor === this.cursor) this.cursor += key.key === "Backspace" ? -1 : 1;
-			this.insert("");
+			await this.insert("");
 		}
-		else if(key.key === "Escape") this.write("");
+		else if(key.key === "Escape") await this.write("");
 
-		// Defines ctrl keys
+		// Defines control keys
 		else if(key.ctrlKey && (key.key === "a" || key.key === "A")) {
 			this.anchor = 0;
 			this.cursor = this.input.length;
@@ -171,7 +241,7 @@ export class ImageTerminal {
 			if(selection === null) return;
 			await navigator.clipboard.writeText(selection.toString());
 		}
-		else if(key.ctrlKey && key.key === "V") this.insert(await navigator.clipboard.readText());
+		else if(key.ctrlKey && key.key === "V") await this.insert(await navigator.clipboard.readText());
 
 		// Defines arrow keys
 		else if(key.key === "ArrowLeft" || key.key === "ArrowRight") {
@@ -181,48 +251,108 @@ export class ImageTerminal {
 		else if(key.key === "ArrowUp" || key.key === "ArrowDown") this.chrono += key.key === "ArrowUp" ? -1 : 1;
 
 		// Defines input keys
-		else if(key.key.length === 1) this.insert(key.key);
+		else if(key.key.length === 1) await this.insert(key.key);
+
+		// Prevents bubble
+		key.preventDefault();
+		key.stopImmediatePropagation();
 	}
 
-	// Retrieves input
-	get input() {
-		// Returns input
-		return this._input;
-	}
-
-	// Inserts input
-	insert(insertion: string): void {
-		// Splits insertion
-		const split = insertion.indexOf("\n");
-		const before = this._input.slice(0, Math.min(this._anchor, this._cursor));
-		const inserted = split === -1 ? insertion : insertion.slice(0, split);
-		const after = this._input.slice(Math.max(this._anchor, this._cursor));
-		const appended = split === -1 ? "" : insertion.slice(split);
-
-		// Writes insertion
-		this.write(before + inserted + after + appended, before.length + inserted.length);
-	}
-
-	// Parses command
-	// @ts-ignore
+	// Executes command
 	static parse(input: string): ImageTerminalCommand {
-		return {
-			command: "",
-			flags: {},
-			parameters: []
+		// Initializes parser
+		const raw = ImageTerminal.purify(input).trimStart();
+		const flags: { [ flags: string ]: string[] } = {};
+		const parameters: string[] = [];
+		const parts: string[] = [];
+		let bucket: string[] = parameters;
+		let pointer = 0;
+
+		// Parses command
+		while(pointer < raw.length) {
+			const ahead = raw.slice(pointer);
+			
+			// Matches spaces
+			const matchSpace = ahead.match(/^\s+/);
+			if(matchSpace !== null) {
+				// Increments
+				pointer += matchSpace[0].length;
+				continue;
+			}
+			
+			// Matches part
+			const matchPart = ahead.match(/^(?:"(?:\\.|[^\\"])*"|(?:\\.|[^\\" ])+)+(?=$|\s)/);
+			if(matchPart !== null) {
+				// Retrieves part
+				const part = matchPart[0].replace(/(?<!\\)"/g, "").replace(/\\(.)/g, "$1");
+
+				// Appends flag
+				if(part[0] === "-") {
+					if(part in flags) throw new ImageTerminalError("Duplicate flag at " + pointer);
+					flags[part] = [];
+					bucket = flags[part];
+				}
+				
+				// Appends paramter
+				else bucket.push(part);
+
+				// Appends part
+				parts.push(part);
+
+				// Increments
+				pointer += matchPart[0].length;
+				continue;
+			}
+
+			// Throws error
+			console.log(ahead);
+			throw new ImageTerminalError("Invalid syntax at " + pointer);
 		}
+
+		// Returns command
+		return {
+			command: parameters.length === 0 ? "" : parameters[0].toLowerCase(),
+			flags: flags,
+			parameters: parameters.slice(1),
+			parts: parts,
+		} satisfies ImageTerminalCommand;
+	}
+
+	// Retrieves pause
+	get pause() {
+		// Returns pause
+		return this._pause;
+	}
+
+	// Modifies pause
+	set pause(pause: boolean) {
+		// Updates pause
+		if(pause === this._pause) return;
+		this._pause = pause;
+		
+		// Renders
+		this._field.style.display = this._pause ? "none" : "block";
 	}
 
 	// Prints content to terminal
 	print(content: any): void {
-		// Initializes log
-		const log = document.createElement("div");
-		log.classList.add("log");
-		log.innerText = String(content);
+		// Prints lines
+		const lines = String(content).split("\n");
+		for(let i = 0; i < lines.length; i++) {
+			const log = document.createElement("div");
+			log.classList.add("log");
+			log.innerText = lines[i];
+			this.frame.insertBefore(log, this._field);
+		}
 		
 		// Renders
-		this.frame.insertBefore(log, this._field);
 		this.bump();
+	}
+
+	// Purifies content into displayable characters
+	static purify(content: string): string {
+		// Returns purified content
+		return content.replace(/[^\na-zA-Z0-9()[\]{}<>'"`!?@#$%^&~_+\-*/=\\|;:,. ]/g, "");
 	}
 
 	// Renders terminal
@@ -240,25 +370,28 @@ export class ImageTerminal {
 	}
 
 	// Runs command
-	// @ts-ignore
 	run(command: ImageTerminalCommand): Promise<void> {
-		const task = (async () => {})();
+		// Runs task
+		if(!this.commands.has(command.command)) throw new ImageTerminalError("Command \"" + command.command + "\" does not exist");
+		const task = this.commands.get(command.command)!(command);
+
+		// Tracks task
 		this._tasks.add(task);
 		task.then(() => this._tasks.delete(task));
+
+		// Returns task
 		return task;
 	}
 
 	// Writes input
-	write(input: string, position: number = input.length): void {
+	async write(input: string, position: number = input.length): Promise<void> {
 		// Updates input
-		const updated = input.replace(/[^\na-zA-Z0-9()[\]{}<>'"`!?@#$%^&~_+\-*/=\\|;:,. ]/g, "");
-		if(this._input === updated) return;
-		const lines = updated.split("\n");
+		const lines = ImageTerminal.purify(input).split("\n");
 		this._input = lines[0];
 
 		// Updates indexes
 		this._chrono = this._history.size;
-		this._cursor = position;
+		this._cursor = Math.min(Math.max(position, 0), input.length);
 		this._anchor = this._cursor;
 		
 		// Handles multi-lined input
@@ -266,15 +399,22 @@ export class ImageTerminal {
 			// Prints input
 			this.print("> " + this._input);
 
-			// Updates history
+			// Analyzes input
 			if(this._input.trim().length != 0) {
+				// Updates history
 				this._history.delete(this._input);
 				this._history.add(this._input);
 				this._timeline = null;
+
+				// Runs command
+				try {
+					await this.run(ImageTerminal.parse(this._input));
+				}
+				catch(error) {
+					if(error instanceof ImageTerminalError) this.print(error.message);
+					else throw error;
+				}
 			}
-			
-			// Runs command
-			this.run(ImageTerminal.parse(this._input));
 
 			// Updates input
 			this._input = lines[i];
